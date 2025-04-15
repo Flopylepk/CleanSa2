ea5b0eae64fb3473c53aec20314c0a80e6aaf6dd
Flopylepk <florencia.torres@davinci.edu.ar>
2025-04-15 10:16:16 -0300

    creacion de variables de opciones de menu

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,15 @@ import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "Hola soy flor");
+		int opcion1=0;
+		int opciongeneral=0;
+		int opcioncliente=0;
+		int opcionadmin=0;
+		int opcionclientes=0;
+		int opcionadminenvios=0;
+		int opcionadminventas=0;
+		
+		
+		
 	}
 }
