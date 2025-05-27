c171041cfcd8d248da1724c4fb13735545483c11
Flopylepk <florencia.torres@davinci.edu.ar>
2025-05-27 16:22:50 -0300

    carritos por estado
    hice que puedas elegir mostrar los carritos según su estado

--- CleanSa17v2/src/DLL/ControllerCarrito.java
+++ CleanSa17v2/src/DLL/ControllerCarrito.java
@@ -41,6 +41,78 @@ public class ControllerCarrito implements  CarritoRepository{
 		}
 		return carrito;
 	}
+	
+	@Override
+	public List<Carrito> mostrarCarritoEnProseso() {
+		LinkedList<Carrito> carrito = new LinkedList<>();
+		try {
+			PreparedStatement stmt = con.prepareStatement("SELECT * FROM carrito where estado=?");
+			stmt.setString(1, "en proseso");
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()) {
+            	int id_carrito=rs.getInt("id_carrito");
+            	Date fecha= rs.getDate("fecha");
+            	String estado=rs.getString("estado");
+            	double total=rs.getDouble("total");
+            	int codigo_envio=rs.getInt("codigo_envio");
+            	int fk_cliente=rs.getInt("fk_cliente");
+            	Carrito carritos=new Carrito(id_carrito, fecha, estado, total, codigo_envio, fk_cliente);
+            	
+            	carrito.add(carritos);
+            }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return carrito;
+	}
+
+	@Override
+	public List<Carrito> mostrarCarritosPagados() {
+		LinkedList<Carrito> carrito = new LinkedList<>();
+		try {
+			PreparedStatement stmt = con.prepareStatement("SELECT * FROM carrito where estado=?");
+			stmt.setString(1, "pagado");
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()) {
+            	int id_carrito=rs.getInt("id_carrito");
+            	Date fecha= rs.getDate("fecha");
+            	String estado=rs.getString("estado");
+            	double total=rs.getDouble("total");
+            	int codigo_envio=rs.getInt("codigo_envio");
+            	int fk_cliente=rs.getInt("fk_cliente");
+            	Carrito carritos=new Carrito(id_carrito, fecha, estado, total, codigo_envio, fk_cliente);
+            	
+            	carrito.add(carritos);
+            }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return carrito;
+	}
+
+	@Override
+	public List<Carrito> mostrarCarritoCancelados() {
+		LinkedList<Carrito> carrito = new LinkedList<>();
+		try {
+			PreparedStatement stmt = con.prepareStatement("SELECT * FROM carrito where estado=?");
+			stmt.setString(1, "cancelado");
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()) {
+            	int id_carrito=rs.getInt("id_carrito");
+            	Date fecha= rs.getDate("fecha");
+            	String estado=rs.getString("estado");
+            	double total=rs.getDouble("total");
+            	int codigo_envio=rs.getInt("codigo_envio");
+            	int fk_cliente=rs.getInt("fk_cliente");
+            	Carrito carritos=new Carrito(id_carrito, fecha, estado, total, codigo_envio, fk_cliente);
+            	
+            	carrito.add(carritos);
+            }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return carrito;
+	}
 
 	@Override
 	public List<Carrito> mostrarCarritoporCliente(int id) {
@@ -48,6 +120,86 @@ public class ControllerCarrito implements  CarritoRepository{
 		try {
 			PreparedStatement stmt = con.prepareStatement("SELECT * FROM carrito where fk_cliente=?");
 			stmt.setInt(1, id);
+			
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()) {
+            	int id_carrito=rs.getInt("id_carrito");
+            	Date fecha= rs.getDate("fecha");
+            	String estado=rs.getString("estado");
+            	double total=rs.getDouble("total");
+            	int codigo_envio=rs.getInt("codigo_envio");
+            	int fk_cliente=rs.getInt("fk_cliente");
+            	Carrito carritos=new Carrito(id_carrito, fecha, estado, total, codigo_envio, fk_cliente);
+            	
+            	carrito.add(carritos);
+            }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return carrito;
+	}
+
+	
+
+	@Override
+	public List<Carrito> mostrarCarritoporClienteEnProseso(int id) {
+		LinkedList<Carrito> carrito = new LinkedList<>();
+		try {
+			PreparedStatement stmt = con.prepareStatement("SELECT * FROM carrito where fk_cliente=? and estado=?");
+			stmt.setInt(1, id);
+			stmt.setString(2, "en proseso");
+			
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()) {
+            	int id_carrito=rs.getInt("id_carrito");
+            	Date fecha= rs.getDate("fecha");
+            	String estado=rs.getString("estado");
+            	double total=rs.getDouble("total");
+            	int codigo_envio=rs.getInt("codigo_envio");
+            	int fk_cliente=rs.getInt("fk_cliente");
+            	Carrito carritos=new Carrito(id_carrito, fecha, estado, total, codigo_envio, fk_cliente);
+            	
+            	carrito.add(carritos);
+            }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return carrito;
+	}
+
+	@Override
+	public List<Carrito> mostrarCarritoporClientePagados(int id) {
+		LinkedList<Carrito> carrito = new LinkedList<>();
+		try {
+			PreparedStatement stmt = con.prepareStatement("SELECT * FROM carrito where fk_cliente=? and estado=?");
+			stmt.setInt(1, id);
+			stmt.setString(2, "pagado");
+			
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()) {
+            	int id_carrito=rs.getInt("id_carrito");
+            	Date fecha= rs.getDate("fecha");
+            	String estado=rs.getString("estado");
+            	double total=rs.getDouble("total");
+            	int codigo_envio=rs.getInt("codigo_envio");
+            	int fk_cliente=rs.getInt("fk_cliente");
+            	Carrito carritos=new Carrito(id_carrito, fecha, estado, total, codigo_envio, fk_cliente);
+            	
+            	carrito.add(carritos);
+            }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return carrito;
+	}
+
+	@Override
+	public List<Carrito> mostrarCarritoporClienteCancelados(int id) {
+		LinkedList<Carrito> carrito = new LinkedList<>();
+		try {
+			PreparedStatement stmt = con.prepareStatement("SELECT * FROM carrito where fk_cliente=? and estado=?");
+			stmt.setInt(1, id);
+			stmt.setString(2, "cancelado");
 			
             ResultSet rs = stmt.executeQuery();
             while(rs.next()) {
--- CleanSa17v2/src/repositorio/CarritoRepository.java
+++ CleanSa17v2/src/repositorio/CarritoRepository.java
@@ -7,5 +7,11 @@ import BLL.*;
 
 public interface CarritoRepository {
 	List<Carrito> mostrarCarrito();
+	List<Carrito> mostrarCarritoEnProseso();
+	List<Carrito> mostrarCarritosPagados();
+	List<Carrito> mostrarCarritoCancelados();
 	List<Carrito> mostrarCarritoporCliente(int id);
+	List<Carrito> mostrarCarritoporClienteEnProseso(int id);
+	List<Carrito> mostrarCarritoporClientePagados(int id);
+	List<Carrito> mostrarCarritoporClienteCancelados(int id);
 }
