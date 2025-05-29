2290dd5b2bd8301d62f5e6caa49f8ee7a3f774da
abrilhayes1 <luz.hayes@davinci.edu.ar>
2025-05-29 16:04:04 -0300

    ControllerEnvio

---
+++ CleanSa17v2/src/DLL/ControllerEnvio.java
@@ -0,0 +1,10 @@
+package DLL;
+
+import java.sql.Connection;
+
+public class ControllerEnvio {
+	private static Connection con = Conexion.getInstance().getConnection();// Poner esto en todos los controladores
+
+	
+	
+}
