0e492280ce218dcb1aef7af0c66f0bb31aec2d9f
abrilhayes1 <luz.hayes@davinci.edu.ar>
2025-06-19 16:19:53 -0300

    .

--- CleanSa17v2/BLL/AdminVentas.java
+++ CleanSa17v2/BLL/AdminVentas.java
@@ -2,14 +2,15 @@ package BLL;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
+import GUI.Main;
 import repositorio.Validador;
 
 public class AdminVentas extends Administrador implements Validador {
 	
+	
 	public AdminVentas(String nombre, String apellido, int id, int tipo, String contrasena) {
 		super(nombre, apellido, id, tipo, contrasena);
 	}
-	/*
 	
 	
 	
@@ -74,7 +75,7 @@ public class AdminVentas extends Administrador implements Validador {
 		
 		}
 		
-		*/
+		
 	
 	
 	
