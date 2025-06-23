e4b782d71c92542e4bd934366344845ca0b13823
abrilhayes1 <luz.hayes@davinci.edu.ar>
2025-06-23 10:38:12 -0300

    vistaCargarProducto
    Terminada la logica para cargar producto.
    Funciona perfecto.

--- CleanSa17v2/src/GUI/vistaCargarProducto.java
+++ CleanSa17v2/src/GUI/vistaCargarProducto.java
@@ -10,6 +10,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import BLL.Categoria;
+import BLL.Producto;
 import DLL.ControllerProducto;
 import DLL.ControllerCategoria;
 
@@ -23,6 +24,7 @@ import java.util.LinkedList;
 import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
 import javax.swing.ImageIcon;
+import java.awt.Color;
 
 public class vistaCargarProducto extends JFrame {
 	private ControllerProducto controller;
@@ -33,6 +35,7 @@ public class vistaCargarProducto extends JFrame {
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JComboBox<Categoria> comboBox;
+	private JLabel Agregado;
 
 	/**
 	 * Launch the application.
@@ -54,7 +57,7 @@ public class vistaCargarProducto extends JFrame {
 	 * Create the frame.
 	 */
 	public vistaCargarProducto() {
-		controller = new ControllerProducto();
+		controller = new ControllerProducto(); // Llama a la clase ControllerProducto para poder usar sus metodos
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -91,9 +94,10 @@ public class vistaCargarProducto extends JFrame {
 		textField_2.setBounds(10, 117, 86, 20);
 		contentPane.add(textField_2);
 		
-		JComboBox comboBox = new JComboBox();
+		comboBox = new JComboBox<>();
 		comboBox.setBounds(10, 164, 105, 22);
 		contentPane.add(comboBox);
+		traerCategoria();
 		
 		JLabel lblCategoria = new JLabel("Categoria");
 		lblCategoria.setBounds(10, 148, 78, 14);
@@ -107,20 +111,45 @@ public class vistaCargarProducto extends JFrame {
 		cargarProducto.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
-					String nombre = textField.getSelectedText();
+					String nombre = textField.getText();
 					double precio = Double.parseDouble(textField_1.getText());
 					int stock = Integer.parseInt(textField_2.getText());
 					Categoria categoriaSeleccionada = (Categoria) comboBox.getSelectedItem();
 					int idCategoria = categoriaSeleccionada.getId_categoria();
+					boolean peligroso = chckbxNewCheckBox.isSelected();
 					
+					int peligrosoInt = peligroso ? 1 : 2; // Lo convierte a boolean
+					
+					if (controller.encontrarProductos(nombre)) { //Implementa el metodo de encontrar productos para validar si existe
+						Agregado.setText("Ya existe un producto con ese nombre.");
+					    Agregado.setForeground(Color.RED);
+					    Agregado.setVisible(true);
+					    return; 
+						
+					}
+					
+					Producto nuevoProducto = new Producto (nombre, precio, stock, idCategoria, peligrosoInt, 0);
+					controller.agregarProducto(nuevoProducto);
+					
+					textField.setText("");
+					textField_1.setText("");
+					textField_2.setText("");
+					comboBox.setSelectedIndex(0);
+					chckbxNewCheckBox.setSelected(false);
+					
+					Agregado.setText("Producto agregado exitosamente.");
+					Agregado.setForeground(Color.GREEN);
+					Agregado.setVisible(true);
 				 
 				} catch (Exception e2) {
-					// TODO: handle exception
+					Agregado.setText("Error al cargar producto.");
+					Agregado.setForeground(Color.RED);
+					Agregado.setVisible(true);
 				}
 				
 			}
 		});
-		cargarProducto.setBounds(169, 229, 129, 23);
+		cargarProducto.setBounds(10, 227, 129, 23);
 		contentPane.add(cargarProducto);
 		
 		JLabel lblNewLabel_1 = new JLabel("");
@@ -128,21 +157,24 @@ public class vistaCargarProducto extends JFrame {
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.TRAILING);
 		lblNewLabel_1.setBounds(117, 0, 309, 137);
 		contentPane.add(lblNewLabel_1);
-	}
-	
-	private void cargarProductos() {
+
+		
+		//Label de mensaje de confirmacion 
+		Agregado = new JLabel("");
+		Agregado.setOpaque(true);                
+		Agregado.setBackground(Color.GRAY);
+		Agregado.setBounds(159, 231, 250, 14); 
+		Agregado.setVisible(false); 
+		contentPane.add(Agregado);
+		
+		
+		
 		
 	}
-	
-	private void limpiarFormulario() {
-		
-	}
-	
-	private void traerCategoria() {
+	private void traerCategoria() { //Trae las categorias desde la base de datos y las junta en una lista para el desplegable
 		ControllerCategoria controllercategoria = new ControllerCategoria();
-		LinkedList<Categoria> categorias = new LinkedList();
+		LinkedList<Categoria> categorias = controllercategoria.mostrarCategorias(); //Guarda las categorias
 		
-		controllercategoria.mostrarCategorias();
 		for (Categoria c : categorias) {
 			comboBox.addItem(c);
 		}
