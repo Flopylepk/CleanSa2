a2214a2b1b50a71b6a3f307d65388fd84db63750
Flopylepk <florencia.torres@davinci.edu.ar>
2025-06-25 14:43:32 -0300

    estado carritos

--- CleanSa17v2/src/DLL/ControllerCarrito.java
+++ CleanSa17v2/src/DLL/ControllerCarrito.java
@@ -210,7 +210,7 @@ public class ControllerCarrito implements  CarritoRepository{
             	double total=rs.getDouble("total");
             	int codigo_envio=rs.getInt("codigoenvio");
             	int fk_cliente=rs.getInt("fk_cliente");
-            	Carrito carritos=new Carrito(id_carrito, fecha, total, codigo_envio, fk_cliente);
+            	Carrito carritos=new Carrito(id_carrito, fecha, estado,total, codigo_envio, fk_cliente);
             	
             	
             	carrito.add(carritos);
@@ -261,7 +261,7 @@ public class ControllerCarrito implements  CarritoRepository{
             	double total=rs.getDouble("total");
             	int codigo_envio=rs.getInt("codigoenvio");
             	int fk_cliente=rs.getInt("fk_cliente");
-            	Carrito carritos=new Carrito(id_carrito, fecha, total, codigo_envio, fk_cliente);
+            	Carrito carritos=new Carrito(id_carrito, fecha, estado,total, codigo_envio, fk_cliente);
             	
             	carrito.add(carritos);
             }
